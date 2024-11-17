bf4b0a8e3766efa0f2dcbf552f94432cc981a015
Samin Chowdhury <s235078@dtu.dk>
2024-11-17 21:51:02 +0100

    Added search in navigation tool

--- app/src/main/java/com/example/movieapp/nav/simplenav.kt
+++ app/src/main/java/com/example/movieapp/nav/simplenav.kt
@@ -1,37 +1,20 @@
 package com.example.movieapp.nav
-import android.os.Bundle
+
 import android.util.Log
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectVerticalDragGestures
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.text.style.TextAlign
+
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.movieapp.repositories.MovieRepository
-import com.example.movieapp.ui.screen.MainScreen
-import com.example.movieapp.ui.screen.FirstTimeScreen
-import com.example.movieapp.ui.screen.LogInScreen
-import com.example.movieapp.ui.screen.SignUpScreen
-import com.example.movieapp.ui.screen.MovieDetailScreen
-import com.example.movieapp.ui.screen.MyFriendsScreen
+import com.example.movieapp.ui.screen.*
 import com.example.movieapp.viewmodels.MovieDetailViewModel
 import com.example.movieapp.viewmodels.MovieViewModel
-import com.example.movieapp.ui.screen.MyList
 
 @Composable
 fun simplenav() {
@@ -55,7 +38,6 @@ fun simplenav() {
                     icon = {}
                 )
                 NavigationBarItem(
-
                     selected = currentRoute == "MyFriendsScreen",
                     onClick = { navController.navigate("MyFriendsScreen") },
                     label = { Text("Friends") },
@@ -65,7 +47,14 @@ fun simplenav() {
                     selected = currentRoute == "MyListScreen",
                     onClick = { navController.navigate("MyListScreen") },
                     label = { Text("My List") },
-                    icon = {} )
+                    icon = {}
+                )
+                NavigationBarItem(
+                    selected = currentRoute == "SearchScreen",
+                    onClick = { navController.navigate("SearchScreen") },
+                    label = { Text("Search") },
+                    icon = {}
+                )
             }
         }
     ) { innerPadding ->
@@ -75,21 +64,17 @@ fun simplenav() {
             modifier = Modifier.padding(innerPadding)
         ) {
             composable("MainScreen") { MainScreen(navController, MovieViewModel(MovieRepository())) }
-            composable("MovieDetailScreen/{id}", arguments = listOf(navArgument("id") {defaultValue = 0})
-            ) {navBackStackEntry ->
-                val id = navBackStackEntry.arguments?.getInt("id") ?:0
-                Log.d("Id","Id: $id")
+            composable(
+                "MovieDetailScreen/{id}",
+                arguments = listOf(navArgument("id") { defaultValue = 0 })
+            ) { navBackStackEntry ->
+                val id = navBackStackEntry.arguments?.getInt("id") ?: 0
+                Log.d("Id", "Id: $id")
                 MovieDetailScreen(id, navController, MovieDetailViewModel(id, MovieRepository()))
             }
-
-
-            composable("MyFriendsScreen") {
-                MyFriendsScreen()
-            }
-
-            composable("MyListScreen") {
-                MyList()
-            }
+            composable("MyFriendsScreen") { MyFriendsScreen() }
+            composable("MyListScreen") { MyList() }
+            composable("SearchScreen") { SearchScreen() }
         }
     }
 }
