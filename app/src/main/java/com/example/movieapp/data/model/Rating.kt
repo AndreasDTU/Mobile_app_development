ea761d4561c3f2df0e1ce27c7142c024244fdf7d
aisha0139 <s235123@dtu.dk>
2025-01-08 14:42:50 +0100

    added profilescreen with user ratings section and created rating data class

---
+++ app/src/main/java/com/example/movieapp/data/model/Rating.kt
@@ -0,0 +1,7 @@
+package com.example.movieapp.data.model
+
+data class Rating(
+    val mediaId: String,
+    val movieTitle: String,
+    val rating: Double
+)
--- app/src/main/java/com/example/movieapp/ui/screen/ratings/ProfileScreen.kt
+++ app/src/main/java/com/example/movieapp/ui/screen/ratings/ProfileScreen.kt
@@ -1,6 +1,8 @@
 package com.example.movieapp.ui.screen.ratings
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -9,6 +11,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.movieapp.viewmodels.UserViewModel
+import com.example.movieapp.data.model.Rating
+
 
 @Composable
 internal fun ProfileScreen(
@@ -16,7 +20,7 @@ internal fun ProfileScreen(
     onEditClick: () -> Unit
 ) {
     val userProfile = userViewModel.userProfile.collectAsState()
-
+    val userRatings = userViewModel.userRatings.collectAsState()
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -34,6 +38,36 @@ internal fun ProfileScreen(
             Button(onClick = onEditClick) {
                 Text(text = "Edit Profile")
             }
+            Spacer (modifier = Modifier.height(24.dp))
+
+            Text(
+                text = "Your Ratings: ",
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+                if (userRatings.value.isNotEmpty()) {
+                    LazyColumn {
+                        items(userRatings.value) { rating ->
+                            RatingItem(rating = rating)
+                        }
+                    }
+                } else {
+                    Text(text = "You have not rated any movies yet.")
+                }
+
         } ?: Text(text = "Loading...")
+    }
+}
+@Composable
+fun RatingItem (rating: Rating) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = rating.movieTitle, style = MaterialTheme.typography.bodyLarge)
+        Text(text = "${rating.rating}/5", style = MaterialTheme.typography.bodyMedium)
     }
 }
