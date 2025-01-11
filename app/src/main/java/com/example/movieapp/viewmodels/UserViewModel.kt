9f17683db9c37d43f0edbaccbdc411b0bcfd6355
aisha0139 <s235123@dtu.dk>
2025-01-11 16:27:26 +0100

    update userviewmodel

--- app/src/main/java/com/example/movieapp/viewmodels/UserViewModel.kt
+++ app/src/main/java/com/example/movieapp/viewmodels/UserViewModel.kt
@@ -1,21 +1,33 @@
 package com.example.movieapp.viewmodels
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.movieapp.data.model.Rating
+import com.example.movieapp.repositories.RatingRepository
 import com.example.movieapp.data.model.UserProfile
 import com.example.movieapp.repositories.UserRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
+
+
 class UserViewModel(
-    private val userRepository: UserRepository = UserRepository()
+    private val userRepository: UserRepository = UserRepository(),
+    private val ratingRepository: RatingRepository = RatingRepository(context)
 ) : ViewModel() {
+
 
     private val _userProfile = MutableStateFlow<UserProfile?>(null)
     val userProfile: StateFlow<UserProfile?> = _userProfile
 
+    private val _userRatings = MutableStateFlow<List<Rating>>(emptyList())
+    val userRatings: StateFlow<List<Rating>> = _userRatings
+
     init {
         fetchUserProfile()
+        loadUserRatings()
+
     }
 
     private fun fetchUserProfile() {
@@ -29,6 +41,27 @@ class UserViewModel(
         viewModelScope.launch {
             userRepository.saveUserProfile(updatedProfile)
             _userProfile.value = updatedProfile
+        }
+    }
+    fun loadUserRatings() {
+        viewModelScope.launch {
+            _userRatings.value = ratingRepository.loadRatings()
+        }
+    }
+
+    fun addOrUpdateRating(mediaId: String, movieTitle: String, rating: Double) {
+        viewModelScope.launch {
+            val currentRatings = _userRatings.value.toMutableList()
+            val existingRatingIndex = currentRatings.indexOfFirst { it.mediaId == mediaId }
+
+            if (existingRatingIndex >= 0) {
+                currentRatings[existingRatingIndex] = Rating(mediaId, movieTitle, rating)
+            } else {
+                currentRatings.add(Rating(mediaId, movieTitle, rating))
+            }
+
+            _userRatings.value = currentRatings
+            ratingRepository.saveRatings(currentRatings)
         }
     }
 }
